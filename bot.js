58e3637face6b73b7522195bff2066ede8e8f57a
Vanix-GOD <75307575+Vanix-GOD@users.noreply.github.com>
2020-12-01 15:31:25 +0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+var Discord = require('discord.js');
+var botName = require('./package.json');
+var bot = new Discord.Client();
+
+bot.on('message', message =>{
+
+    // Variables
+    var sender = message.author; //The person who sent the message
+    var msg = message.content;
+    var prefix = "-" //The text before commands, you can put anything that you prefer
+
+    if(message.author.id != "783225162777755649" && message.channel.id === "575624302238498817"){
+        if(msg.startsWith('-', 0)){
+            if(msg === prefix + "Hi" && message.channel.id === "575624302238498817"){
+                message.channel.send('Hello My Friend') // Sends a message to the channel, with the content
+            }
+        }
+        }
+    }
+});
+
+bot.login(process.env.BOT_TOKEN)
